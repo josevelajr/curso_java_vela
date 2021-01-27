01ce030f2552b15b7fe7dc665c0e6f4ed6899bf8
jvelajr <jose.vela.jr@outlook.com>
2021-01-26 23:33:12 -0300

    Estrutura Condicional - Exercicio35

---
+++ src/estruturascondicionais/Exercicio35.java
@@ -0,0 +1,20 @@
+package estruturascondicionais;
+
+import java.util.Scanner;
+
+public class Exercicio35 {
+    public void exercicio35(){
+        int horas;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Por favor digite as horas: ");
+        horas = sc.nextInt();
+
+        if (horas < 12) {
+            System.out.println("Bom dia");
+        } else if (horas < 18) {
+            System.out.println("Boa tarde");
+        } else {
+            System.out.println("Boa noite");
+        }
+    }
+}
